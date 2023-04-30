103b9bb9a8700dd83da0ba8f12b82cfe1a448f4c
Rustam Zeinalov <comecur@mail.ru>
2023-04-30 17:14:35 +0300

    feat: add eslint-config-airbnb-base to esLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
--- modules/htmlElements.js
+++ modules/htmlElements.js
@@ -1,18 +1,15 @@
-export class СreateHtml {
-  constructor(){
-    const {body} = document;
-    const wrapper = this.wrapper.CreateElement ('div');
-    const title = this.title.CreateElement ('h1');
-    const textArea = this.textArea.CreateElement ('textarea');
-    const keyBoard = this.keyBoard.CreateElement ('section');
-
-    wrapper.classList.add ('wrapper');
-    title.classList.add ('header__title');
-    textArea.classList.add ('input__text-keyboard')
-    keyBoard.classList.add ('keyboard')
-
+class СreateHtml {
+  constructor() {
+    const { body } = document;
+    const wrapper = this.wrapper.CreateElement('div');
+    const title = this.title.CreateElement('h1');
+    const textArea = this.textArea.CreateElement('textarea');
+    const keyBoard = this.keyBoard.CreateElement('section');
+    wrapper.classList.add('wrapper');
+    title.classList.add('header__title');
+    textArea.classList.add('input__text-keyboard');
+    keyBoard.classList.add('keyboard');
     body.appendChild(wrapper);
-    wrapper.append (title,textArea,keyBoard);
-
+    wrapper.append(title, textArea, keyBoard);
   }
 }
