d1fe18ccd09a8df4938cc8b08d6337152f1c757c
Rustam Zeinalov <comecur@mail.ru>
2023-05-02 01:10:22 +0300

    refactor: add new desighn for keyboard

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import CreateHtml from './modules/htmlElements.js';
 
+console.log(CreateHtml);
 const textarea = document.getElementById('textarea');
 const virtualKeyboard = document.querySelector('.keyboard');
 
@@ -8,9 +9,9 @@ virtualKeyboard.addEventListener('click', (event) => {
   if (!key) return;
   const { char } = key.dataset;
   textarea.value += char;
+  textarea.focus();
 });
 
-textarea.addEventListener('keydown', (event) => {
-  const char = event.key;
-  textarea.value += char;
+document.addEventListener('DOMContentLoaded', () => {
+  textarea.focus();
 });
